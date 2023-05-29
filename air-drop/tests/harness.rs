016d473091e63e52f4287a0ec36a49ee5125524c
t4sk <tsk.nakamura@gmail.com>
2023-05-29 09:24:06 +0900

    air-drop - test

--- air-drop/tests/harness.rs
+++ air-drop/tests/harness.rs
@@ -3,7 +3,54 @@ use fuels::{prelude::*, types::Bits256, types::Identity};
 mod set_up;
 mod tree;
 
-use set_up::set_up;
+use set_up::{set_up, InitEvent, ClaimEvent};
+
+#[tokio::test]
+async fn test_init() {
+    let (instance, id, wallets) = set_up().await;
+
+    let asset_id = ContractId::new(BASE_ASSET_ID.try_into().unwrap());
+
+    // size = 32, elements = 1 of type u8
+    let merkle_root = Bits256([1u8; 32]);
+    let num_leaves = 4;
+
+    let call_params = CallParameters::default().set_amount(1000);
+
+    let res = instance
+        .methods()
+        .init(merkle_root, num_leaves)
+        .call_params(call_params)
+        .unwrap()
+        .call()
+        .await
+        .unwrap();
+
+    let logs = res.decode_logs_with_type::<InitEvent>().unwrap();
+    let event = logs.get(0).unwrap();
+
+    assert_eq!(
+        *event,
+        InitEvent {
+            asset: asset_id,
+            merkle_root,
+            num_leaves
+        }
+    );
+    
+    assert_eq!(
+        instance.methods().asset().call().await.unwrap().value.unwrap(),
+        asset_id
+    );
+    assert_eq!(
+        instance.methods().merkle_root().call().await.unwrap().value.unwrap(),
+        merkle_root
+    );
+    assert_eq!(
+        instance.methods().num_leaves().call().await.unwrap().value,
+        num_leaves
+    );
+}
 
 #[tokio::test]
 async fn test_claim() {
@@ -23,9 +70,6 @@ async fn test_claim() {
 
     let (merkle_tree, merkle_root, merkle_leaf, proof) = tree::build(index, leaves);
 
-    // size = 32, elements = 1 of type u8
-    // let root = Bits256([1u8; 32]);
-
     let call_params = CallParameters::default().set_amount(1000);
 
     instance
@@ -36,6 +80,8 @@ async fn test_claim() {
         .call()
         .await
         .unwrap();
+
+    // let bal = wallet.get_asset_balance(asset).await.unwrap()
 
     instance
         .with_account(sender.clone())
