c9ababfd4b659ef92ae8605ea910b64476a3a855
t4sk <tsk.nakamura@gmail.com>
2023-06-11 09:43:11 +0900

    test signatures

--- sig/src/main.sw
+++ sig/src/main.sw
@@ -5,22 +5,28 @@ use std::{bytes::Bytes, b512::B512, hash::{sha256}, ecr::{ec_recover_address}};
 struct SignParams {
     contract_id: ContractId,
     z: u64,
-    // data: Bytes,
 }
 
 abi MyContract {
+    fn get_data_hash(data: Bytes) -> b256;
+
     fn get_msg_hash(x: u64, y: u64, params: SignParams, data: Bytes) -> b256;
 
     fn recover(sig: B512, x: u64, y: u64, params: SignParams, data: Bytes) -> Address;
 }
 
 impl MyContract for Contract {
+    fn get_data_hash(data: Bytes) -> b256 {
+        // https://github.com/FuelLabs/sway/issues/3809
+        data.sha256()
+    }
+
     fn get_msg_hash(x: u64, y: u64, params: SignParams, data: Bytes) -> b256 {
-        sha256((x, y, params, data))
+        sha256((x, y, params, data.sha256()))
     }
 
     fn recover(sig: B512, x: u64, y: u64, params: SignParams, data: Bytes) -> Address {
-        let msg_hash = sha256((x, y, params, data));
+        let msg_hash = sha256((x, y, params, data.sha256()));
         let signer = ec_recover_address(sig, msg_hash).unwrap().value;
         Address::from(signer)
     }
--- sig/tests/harness.rs
+++ sig/tests/harness.rs
@@ -7,6 +7,8 @@ use fuels::{
     types::{Bits256, B512},
 };
 
+use sha2::{Digest, Sha256};
+
 // Load abi from json
 abigen!(Contract(
     name = "MyContract",
@@ -44,14 +46,24 @@ async fn test_sig() {
 
     let x: u64 = 2;
     let y: u64 = 3;
-    let data = Bytes(vec![1u64, 2u64, 3u64]);
-    println!("data {:?}", data);
+    let data = Bytes(vec![1,2,3]);
     let params = SignParams {
         contract_id: inst_id,
         z: 123,
-        // data
     };
-    let data_bytes = calldata!((x, y, params.clone(), data.clone()));
+
+    // Hash data
+    let mut hasher = Sha256::new();
+    hasher.update(data.0.clone());
+    let data_hash: [u8; 32] = hasher.finalize().try_into().unwrap();
+
+    let res = inst.methods().get_data_hash(data.clone()).call().await.unwrap();
+    println!("data:  {:?}", data);
+    println!("data hash: {:?}", data_hash);
+    println!("res:       {:?}", res.value);
+
+    let data_bytes = calldata!((x, y, params.clone(), res.value.clone()));
+    // println!("calldata {:?}", data_bytes.clone());
 
     // Sign
     let sig = wallet.sign_message(&data_bytes).await.unwrap();
@@ -62,7 +74,7 @@ async fn test_sig() {
     ));
 
     // Recover
-    // NOTE - date is hashed
+    // NOTE - data_bytes is hashed by Message::new
     let msg_hash = fuel_crypto::Message::new(data_bytes);
     let rec = sig.recover(&msg_hash).unwrap();
 
